d078d883ac12511270e6417bab8e727e3af80c5b
Marc Littlemore <marc.littlemore@bbc.co.uk>
2015-02-27 09:30:45 +0000

    Removed components for now.

--- public/app/components/WeatherApp.js
+++
@@ -1,9 +0,0 @@
-var WeatherApp = React.createClass({
-    render: function(
-        var div = React.createFactory('div');
-        
-        return div(null, React.DOM.h1('WeatherApp'));
-    )
-});
-
-return WeatherApp;
